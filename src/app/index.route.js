6abbd25410c59ae70046358f6587a81a2b485f4c
mhigbyflowers <mhigbyflowers@gmail.com>
2017-06-22 17:25:33 -0400

    Masonry grid works

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -12,7 +12,91 @@
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
-        controllerAs: 'main'
+        controllerAs: 'ctrl'
+      })
+      .state('equal parts', {
+        url: '/equal-parts',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('osage', {
+        url: '/osage',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('ripe/raw', {
+        url: '/ripe-raw',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('mirror object', {
+        url: '/mirror-object',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('near earth objects', {
+        url: '/near-earth-objects',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('levarche', {
+        url: '/levarche',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('table (two)', {
+        url: '/table-two',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('table (one)', {
+        url: '/table-one',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('outside', {
+        url: '/outside',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('float pile', {
+        url: '/float-pile',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('primary ornament', {
+        url: '/primary-ornament',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('alienum', {
+        url: '/alienum',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('through an eye', {
+        url: '/through-an-eye',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
+      })
+      .state('objects', {
+        url: '/objects',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'ctrl'
       });
 
     $urlRouterProvider.otherwise('/');
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,14 +6,15 @@
       .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController($timeout, webDevTec, toastr) {
+    function MainController() {
       var vm = this;
       vm.equalPartsMarkup = "<div id='text'><p><em>Equal Parts</em> at The Packing Plant, Nashville, TN, October 2015</p><p>'For<em> Equal Parts </em>Virginia Griswold continues her exploration of multiples, pulling ideas from two different bodies of work to create new groupings. This work culminates in a sculptural tableau vivant composed of specimens, fragments, and forms from her studio. Bringing together sculpture, casting, and photography Virginia finds connections between her personal practice and the metaphorical act of ‘match-making’, constructing relationships between process and product that disrupt a dichotomous understanding of form. At once fleshy and plastic, floral and mineral, Griswold’s objects distort polarized perceptions like natural/unnatural and finished/unfinished, exposing the falsity of simple binaries.' </p><p>- Zack Rafuls and Ann Catherine Carter, curators and co-directors - <a href='http://thepackingplant.net/'>The Packing Plant</a></p><p><a href='http://badatsports.com/2015/from-the-road-transformer-2/'>READ A REVIEW OF <em>Equal Parts</em> BY ERIC ASBOE FOR <em>BAD AT SPORTS</em></a><br /></p></div>";
+
         vm.projects = [{
             name: 'equal parts',
             year: '2015',
             desc: vm.equalPartsMarkup,
-            img:['ep-1.jpg' , 'ep-10.jpg' , 'ep-11.jpg' , 'p-12.jpg' , 'ep-2.jpg' , 'ep-3.jpg' , 'ep-4.jpg' , 'ep-5.jpg' , 'ep-6.jpg' , 'ep-7.jpg' , 'ep-8.jpg' , 'ep-9.jpg']
+            img:['ep-1.jpg' , 'ep-10.jpg' , 'ep-11.jpg' , 'ep-12.jpg' , 'ep-2.jpg' , 'ep-3.jpg' , 'ep-4.jpg' , 'ep-5.jpg' , 'ep-6.jpg' , 'ep-7.jpg' , 'ep-8.jpg' , 'ep-9.jpg']
 
           },
           {
@@ -34,11 +35,11 @@
               name: 'mirror object',
               year: '2014',
               desc: 'A site specific installation in Iceland',
-              materials: 'digital video',
-              img:['VIDEO']
+              materials: 'digital video'
+              // img:['VIDEO']
             },
             {
-              name: 'near earth bjects',
+              name: 'near earth objects',
               year: '2014',
               desc: 'Trahern Gallery, Austin Peay State University, Fall 2014',
               img:['neo-1.jpg' , 'neo-10.jpg' ,'neo-11.png','neo-2.jpg' , 'neo-3.jpg' , 'neo-4.jpg' , 'neo-5.jpg' , 'neo-6.jpg' , 'neo-7.jpg' , 'neo-8.jpg' , 'neo-9.jpg']
@@ -70,28 +71,28 @@
               year: '2011',
               desc: '(a thaw, an overflow)  Alfred, NY, (site-influenced)',
               materials: 'plaster, foam, milk glass lid',
-              img:[]
+              img:['o-1.jpg', 'o-2.jpg', 'o-3.jpg','o-4.jpg']
             },
             {
               name: 'float pile',
               year: '2011',
               desc: '',
               materials: ' expandable marine foam, pigments, rubber, gold dust, handmade dolly, casters, t-pins',
-              img:['o-1.jpg', 'o-2.jpg', 'o-3.jpg', 'o-4.jpg']
+              img:['fp-1.jpg', 'fp-2.jpg', 'fp-3.jpg']
             },
             {
               name: 'primary ornament',
               year: '2010',
               desc: '',
               materials: 'slip-cast ceramic, porcelain shards, glass, oak, plexi-glass',
-              img:[]
+              img:['po-1.jpg', 'po-2.jpg', 'po-3.jpg']
             },
             {
               name: 'alienum',
               year: '2010',
               desc: '',
               materials: 'slip-cast porcelain, rubber, plaster, steel',
-              img:[]
+              img:['al-1.jpg', 'al-2.jpg', 'al-3.jpg']
             },
             {
               name: 'through an eye',
@@ -109,6 +110,9 @@
 'obj-10.jpg','obj-11.jpg', 'obj-12.jpg','obj-13.jpg' ]
             }
 
-          ]
+          ];
+
+          vm.allImages= ['ep-1.jpg' , 'ep-10.jpg' , 'ep-11.jpg' , 'ep-12.jpg' , 'ep-2.jpg' , 'ep-3.jpg' , 'ep-4.jpg' , 'ep-5.jpg' , 'ep-6.jpg' , 'ep-7.jpg' , 'ep-8.jpg' , 'ep-9.jpg','os-1.jpg' , 'os-2.jpg' , 'os-3.jpg' , 'os-4.jpg' , 'os-5.jpg' , 'os-6.jpg' , 'os-7.jpg','rr-1.png', 'rr-2.png', 'rr-3.jpg', 'rr-4.jpg', 'rr-5.jpg', 'rr-6.jpg', 'rr-7.jpg', 'rr-8.png','neo-1.jpg' , 'neo-10.jpg' ,'neo-11.png','neo-2.jpg' , 'neo-3.jpg' , 'neo-4.jpg' , 'neo-5.jpg' , 'neo-6.jpg' , 'neo-7.jpg' , 'neo-8.jpg' , 'neo-9.jpg','lev-1.jpg' ,'lev-2.jpg' ,'lev-3.jpg' ,'lev-4.jpg' ,'lev-5.jpg','t2-1.jpg' , 't2-2.jpg' , 't2-3.jpg' , 't2-4.jpg' , 't2-5.jpg','t1-1.jpg' , 't1-2.jpg' ,'t1-3.jpg','o-1.jpg', 'o-2.jpg', 'o-3.jpg','o-4.jpg','fp-1.jpg', 'fp-2.jpg', 'fp-3.jpg','po-1.jpg', 'po-2.jpg', 'po-3.jpg','al-1.jpg', 'al-2.jpg', 'al-3.jpg','tae-1.jpg' ,'tae-2.jpg','obj-1.jpg' , 'obj-2.jpg' , 'obj-3.jpg' ,'obj-4.jpg' , 'obj-5.jpg' ,'obj-6.jpg' , 'obj-7.jpg' ,'obj-8.jpg' , 'obj-9.jpg',
+          'obj-10.jpg','obj-11.jpg', 'obj-12.jpg','obj-13.jpg']
         }
     })();
