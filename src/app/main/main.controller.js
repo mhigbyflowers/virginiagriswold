6944992970cfa54305e40a968e9564a3f766eb70
mhigbyflowers <mhigbyflowers@gmail.com>
2017-06-24 13:17:10 -0400

    wip

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -114,5 +114,11 @@
 
           vm.allImages= ['ep-1.jpg' , 'ep-10.jpg' , 'ep-11.jpg' , 'ep-12.jpg' , 'ep-2.jpg' , 'ep-3.jpg' , 'ep-4.jpg' , 'ep-5.jpg' , 'ep-6.jpg' , 'ep-7.jpg' , 'ep-8.jpg' , 'ep-9.jpg','os-1.jpg' , 'os-2.jpg' , 'os-3.jpg' , 'os-4.jpg' , 'os-5.jpg' , 'os-6.jpg' , 'os-7.jpg','rr-1.png', 'rr-2.png', 'rr-3.jpg', 'rr-4.jpg', 'rr-5.jpg', 'rr-6.jpg', 'rr-7.jpg', 'rr-8.png','neo-1.jpg' , 'neo-10.jpg' ,'neo-11.png','neo-2.jpg' , 'neo-3.jpg' , 'neo-4.jpg' , 'neo-5.jpg' , 'neo-6.jpg' , 'neo-7.jpg' , 'neo-8.jpg' , 'neo-9.jpg','lev-1.jpg' ,'lev-2.jpg' ,'lev-3.jpg' ,'lev-4.jpg' ,'lev-5.jpg','t2-1.jpg' , 't2-2.jpg' , 't2-3.jpg' , 't2-4.jpg' , 't2-5.jpg','t1-1.jpg' , 't1-2.jpg' ,'t1-3.jpg','o-1.jpg', 'o-2.jpg', 'o-3.jpg','o-4.jpg','fp-1.jpg', 'fp-2.jpg', 'fp-3.jpg','po-1.jpg', 'po-2.jpg', 'po-3.jpg','al-1.jpg', 'al-2.jpg', 'al-3.jpg','tae-1.jpg' ,'tae-2.jpg','obj-1.jpg' , 'obj-2.jpg' , 'obj-3.jpg' ,'obj-4.jpg' , 'obj-5.jpg' ,'obj-6.jpg' , 'obj-7.jpg' ,'obj-8.jpg' , 'obj-9.jpg',
           'obj-10.jpg','obj-11.jpg', 'obj-12.jpg','obj-13.jpg']
+
+        
+
+
         }
+
+
     })();
